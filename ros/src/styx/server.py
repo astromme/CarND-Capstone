73426a13c9d660ad5e8f634723f2b5b9812f60ba
John Lees-Miller <jdleesmiller@gmail.com>
2017-09-09 22:36:34 +0100

    merge fixes from jdleesmiller

--- ros/src/styx/server.py
+++ ros/src/styx/server.py
@@ -11,7 +11,14 @@ from conf import conf
 
 sio = socketio.Server()
 app = Flask(__name__)
-msgs = []
+bridge = Bridge(conf)
+
+#
+# JLM: Changed to only send the latest message for each topic, rather
+# than queuing out of date messages. Based on
+# https://github.com/amakurin/CarND-Capstone/commit/9809bc60d51c06174f8c8bfe6c40c88ec1c39d50
+#
+msgs = {}
 
 dbw_enable = False
 
@@ -20,11 +27,9 @@ def connect(sid, environ):
     print("connect ", sid)
 
 def send(topic, data):
-    s = 1
-    msgs.append((topic, data))
-    #sio.emit(topic, data=json.dumps(data), skip_sid=True)
+    msgs[topic] = data
 
-bridge = Bridge(conf, send)
+bridge.register_server(send)
 
 @sio.on('telemetry')
 def telemetry(sid, data):
@@ -34,7 +39,7 @@ def telemetry(sid, data):
         bridge.publish_dbw_status(dbw_enable)
     bridge.publish_odometry(data)
     for i in range(len(msgs)):
-        topic, data = msgs.pop(0)
+        topic, data = msgs.popitem()
         sio.emit(topic, data=data, skip_sid=True)
 
 @sio.on('control')
